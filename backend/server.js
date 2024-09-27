5543e95cd19f87b5c6a806a0c04ff0a3e4b73ea6
Olayiwolaaa <muizzkara91@gmail.com>
2024-09-27 04:29:40 +0100

    successfully connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 // import { json } from "body-parser";
-import session from "express-session";
-import passport from "passport";
-import { Strategy as GitHubStrategy } from "passport-github2";
-import mongoose from "mongoose";
+// import session from "express-session";
+// import passport from "passport";
+// import { Strategy as GitHubStrategy } from "passport-github2";
+// import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -13,10 +13,8 @@ const app = express();
 
 app.use(express.json());
 
-app.listen(8080, () => {
+const port = process.env.PORT || 2400;
+app.listen(port, () => {
+    connectDB();
     console.log("Server running on port http://localhost:8080");
 });
-
-
-// password
-// ELobB2WsuVvA0mTo;
